4626e5a6eaf29f0aecb0dbd4c3d8ff36e363bc54
Rahofski <157171930+Rahofski@users.noreply.github.com>
2024-08-13 23:17:59 +0300

    Update script.js
    очки

--- script.js
+++ script.js
@@ -2,29 +2,37 @@ const dino = document.getElementById("dino");
 
 const cactus = document.getElementById("cactus");
 
+let points = 0;
 
-document.addEventListener("keydown", function(event)
+let rec = 0;
+document.addEventListener("keydown",function(event)
 {
     jump();
+    document.getElementById('point').innerHTML = `${points}`;
 })
 
-document.addEventListener("mousedown", function(event)
-{
-    jump();
-})
+
 
 function jump () {
     if (dino.classList != "jump") {dino.classList.add("jump");}
     setTimeout (function() 
     {
         dino.classList.remove("jump");
-    }, 300)
+    }, 300);
+    points++;
 }
 
 let isAlive = setInterval(function(){
 
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 150) alert ("YEAH"); 
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) 
+        {
+            if (points > rec ) rec = points;
+            document.getElementById('rec').innerHTML = `${rec}`;
+            points = 0;
+            document.getElementById('point').innerHTML = `${points}`;
+            alert ("YEAH");
+        } 
 
 })
